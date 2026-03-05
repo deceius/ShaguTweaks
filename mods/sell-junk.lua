2b3132e01592393e94483da0d599cde4e33a07bd
Julius Decena <julius.decena@outlook.com>
2026-03-05 11:53:07 +0800

    Update sell-junk.lua

--- mods/sell-junk.lua
+++ mods/sell-junk.lua
@@ -14,46 +14,64 @@ local processed = {}
 
 local function CreateGoldString(money)
   if type(money) ~= "number" then return "-" end
-
   local gold = floor(money/ 100 / 100)
   local silver = floor(mod((money/100),100))
   local copper = floor(mod(money,100))
-
-  local string = ""
-  if gold > 0 then string = string .. "|cffffffff" .. gold .. "|cffffd700g" end
-  if silver > 0 or gold > 0 then string = string .. "|cffffffff " .. silver .. "|cffc7c7cfs" end
-  string = string .. "|cffffffff " .. copper .. "|cffeda55fc"
-
-  return string
+  local str = ""
+  if gold > 0 then str = str .. "|cffffffff" .. gold .. "|cffffd700g" end
+  if silver > 0 or gold > 0 then str = str .. "|cffffffff " .. silver .. "|cffc7c7cfs" end
+  str = str .. "|cffffffff " .. copper .. "|cffeda55fc"
+  return str
 end
 
 local function HasGreyItems()
-  for bag = 0, 4, 1 do
-    for slot = 1, GetContainerNumSlots(bag), 1 do
-      local name = GetContainerItemLink(bag,slot)
-      if name and string.find(name,"ff9d9d9d") then return true end
+  for bag = 0, 4 do
+    for slot = 1, GetContainerNumSlots(bag) do
+      local name = GetContainerItemLink(bag, slot)
+      if name and string.find(name, "ff9d9d9d") then return true end
     end
   end
   return nil
 end
 
 local function GetNextGreyItem()
-  for bag = 0, 4, 1 do
-    for slot = 1, GetContainerNumSlots(bag), 1 do
-      local name = GetContainerItemLink(bag,slot)
-      if name and string.find(name,"ff9d9d9d") and not processed[bag.."x"..slot] then
+  for bag = 0, 4 do
+    for slot = 1, GetContainerNumSlots(bag) do
+      local name = GetContainerItemLink(bag, slot)
+      if name and string.find(name, "ff9d9d9d") and not processed[bag.."x"..slot] then
         processed[bag.."x"..slot] = true
         return bag, slot
       end
     end
   end
-
   return nil, nil
 end
 
 module.enable = function(self)
-  local autovendor = CreateFrame("Frame", nil, nil)
+  local autovendor = CreateFrame("Frame", nil, MerchantFrame)
   autovendor:Hide()
+
+  -- Create the button using CheckButton to support Disable/Enable states
+  autovendor.button = CreateFrame("CheckButton", "ShaguSellJunkButton", MerchantFrame, "ItemButtonTemplate")
+  autovendor.button:SetWidth(34)
+  autovendor.button:SetHeight(34)
+  
+  -- Tooltip logic
+  autovendor.button:SetScript("OnEnter", function()
+    GameTooltip:SetOwner(this, "ANCHOR_RIGHT")
+    GameTooltip:SetText(T["Sell Grey Items"])
+    GameTooltip:Show()
+  end)
+  autovendor.button:SetScript("OnLeave", function() GameTooltip:Hide() end)
+
+  -- Hide the default blue border
+  local normalTex = autovendor.button:GetNormalTexture()
+  if normalTex then normalTex:SetAlpha(0) end
+
+  -- Set Icon
+  local btnIcon = getglobal(autovendor.button:GetName().."IconTexture")
+  btnIcon:SetTexture("Interface\\Icons\\inv_misc_coin_06")
+  btnIcon:SetAllPoints(autovendor.button)
 
   autovendor:SetScript("OnShow", function()
     processed = {}
@@ -68,104 +86,78 @@ module.enable = function(self)
   end)
 
   autovendor:SetScript("OnUpdate", function()
-    -- throttle to to one item per .1 second
-    if ( this.tick or 1) > GetTime() then return else this.tick = GetTime() + .1 end
-
-    -- scan for the next grey item
+    if (this.tick or 1) > GetTime() then return else this.tick = GetTime() + .1 end
     local bag, slot = GetNextGreyItem()
     if not bag or not slot then
       this:Hide()
+      -- Disable button once finished
+      autovendor.button:Disable()
+      btnIcon:SetDesaturated(true)
+      yellowBorder:SetVertexColor(0.5, 0.5, 0.5, 1)
       return
     end
-
-    -- double check to only sell grey
-    local name = GetContainerItemLink(bag,slot)
-    if not name or not string.find(name,"ff9d9d9d") then
-      return
-    end
-
-    -- get value
+    
     local _, icount = GetContainerItemInfo(bag, slot)
     local itemID = GetItemIDFromLink(GetContainerItemLink(bag, slot))
-    local price = ShaguTweaks.SellValueDB[itemID] or 0
-    if this.price then
-      this.price = this.price + ( price * ( icount or 1 ) )
-      this.count = this.count + 1
-    end
-
-    -- abort if the merchant window disappeared
-    if not this.merchant then return end
-
-    -- clear cursor and sell the item
+    local price = ShaguTweaks.SellValueDB and ShaguTweaks.SellValueDB[itemID] or 0
+    this.price = this.price + (price * (icount or 1))
+    this.count = this.count + 1
+    
     ClearCursor()
     UseContainerItem(bag, slot)
   end)
 
   autovendor:RegisterEvent("MERCHANT_SHOW")
   autovendor:RegisterEvent("MERCHANT_CLOSED")
-  autovendor:RegisterEvent("MERCHANT_UPDATE")
   autovendor:SetScript("OnEvent", function()
-    autovendor.button:Update()
+    if event == "MERCHANT_SHOW" then
+      if MerchantRepairText then MerchantRepairText:SetText("") end
+      if MerchantRepairAllButton then
+        MerchantRepairAllButton:SetWidth(36)
+        MerchantRepairAllButton:SetHeight(36)
+      end
+      if MerchantRepairItemButton then
+        MerchantRepairItemButton:SetWidth(36)
+        MerchantRepairItemButton:SetHeight(36)
+      end
 
-    if event == "MERCHANT_CLOSED" then
-      autovendor.merchant = nil
-      autovendor:Hide()
-    elseif event == "MERCHANT_SHOW" then
-      autovendor.merchant = true
+      autovendor.button:ClearAllPoints()
+      if MerchantRepairItemButton:IsShown() then
+        autovendor.button:SetPoint("RIGHT", MerchantRepairItemButton, "LEFT", -4, 0)
+      else
+        autovendor.button:SetPoint("RIGHT", MerchantBuyBackItemItemButton, "LEFT", -14, 0)
+      end
+
       autovendor.button:Show()
+      if HasGreyItems() then
+        autovendor.button:Enable()
+        btnIcon:SetDesaturated(false)
+      else
+        autovendor.button:Disable()
+        btnIcon:SetDesaturated(true)
+      end
+
+    elseif event == "MERCHANT_CLOSED" then
+      autovendor:Hide()
     end
-
-    MerchantRepairText:SetText("")
-    if MerchantRepairItemButton:IsShown() then
-      autovendor.button:ClearAllPoints()
-      autovendor.button:SetPoint("RIGHT", MerchantRepairItemButton, "LEFT", -4, 0)
-    else
-      autovendor.button:ClearAllPoints()
-      autovendor.button:SetPoint("RIGHT", MerchantBuyBackItemItemButton, "LEFT", -14, 0)
-    end
-  end)
-
-  -- Setup Autosell button
-  autovendor.button = CreateFrame("Button", nil, MerchantFrame)
-  autovendor.button:SetWidth(36)
-  autovendor.button:SetHeight(36)
-  autovendor.button:SetNormalTexture("Interface\\Buttons\\UI-Quickslot2")
-  autovendor.button:SetNormalTexture("Interface\\Icons\\Spell_Shadow_SacrificialShield")
-  autovendor.button:SetScript("OnEnter", function()
-    GameTooltip:SetOwner(this, "ANCHOR_RIGHT")
-    GameTooltip:SetText(T["Sell Grey Items"])
-    GameTooltip:Show()
-  end)
-
-  autovendor.button:SetScript("OnLeave", function()
-    GameTooltip:Hide()
   end)
 
   autovendor.button:SetScript("OnClick", function()
     autovendor:Show()
   end)
 
-  autovendor.button.Update = function()
-    if not autovendor:IsVisible() then
-      if HasGreyItems() then
-        autovendor.button:Enable()
-        autovendor.button:GetNormalTexture():SetDesaturated(false)
-      else
-        autovendor.button:Disable()
-        autovendor.button:GetNormalTexture():SetDesaturated(true)
-      end
-    else
-      autovendor.button:Disable()
-      autovendor.button:GetNormalTexture():SetDesaturated(true)
-    end
-  end
-
-  -- Hook MerchantFrame_Update
   if not HookMerchantFrame_Update then
     local HookMerchantFrame_Update = MerchantFrame_Update
     function _G.MerchantFrame_Update()
       if MerchantFrame.selectedTab == 1 then
         autovendor.button:Show()
+        if HasGreyItems() then
+          autovendor.button:Enable()
+          btnIcon:SetDesaturated(false)
+        else
+          autovendor.button:Disable()
+          btnIcon:SetDesaturated(true)
+        end
       else
         autovendor.button:Hide()
       end
