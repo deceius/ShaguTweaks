1ee005adeadc311618695c16cfac9de9f51c2288
Julius Decena <julius.decena@outlook.com>
2026-03-21 15:28:34 +0800

    Update reduced-actionbar.lua

--- mods/reduced-actionbar.lua
+++ mods/reduced-actionbar.lua
@@ -106,8 +106,8 @@ module.enable = function(self)
   MainMenuBarTexture1:SetPoint("RIGHT", MainMenuBarArtFrame, "RIGHT")
 
   -- move gryphon textures
-  MainMenuBarLeftEndCap:SetPoint("RIGHT", MainMenuBarArtFrame, "LEFT", 29.5, 0)
-  MainMenuBarRightEndCap:SetPoint("LEFT", MainMenuBarArtFrame, "RIGHT", -31.5, 0)
+  MainMenuBarLeftEndCap:SetPoint("RIGHT", MainMenuBarArtFrame, "LEFT", 32, 0)
+  MainMenuBarRightEndCap:SetPoint("LEFT", MainMenuBarArtFrame, "RIGHT", -32, 0)
 
   -- move MultiBarBottomRight ontop of MultiBarBottomLeft
   MultiBarBottomRight:ClearAllPoints()
